a1d3c95451d1dc391ae6dcceb16e10f428e18a87
wesbin <tktma12>
2020-05-12 13:00:10 +0900

    7강

--- webpack/source/index.js
+++ webpack/source/index.js
@@ -1,3 +1,6 @@
-import hello_word from '../source/hello.js'
-import world_word from '../source/world.js'
-document.querySelector('#root').innerHTML = hello_word + ', ' + world_word;
+import hello_word from './hello.js';
+import world_word from './world.js';
+
+import css from './style.css';
+
+document.querySelector('#root').innerHTML = hello_word + ', ' + world_word + '[index]';
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,9 +2,24 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './source/index.js',
+    entry: {
+        index: './source/index.js',
+        about: './source/about.js'
+    },
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'index_bundle.js'
+        filename: '[name]_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [
+                    //뒤 쪽에 있는 로더가 먼저 실행된다.
+                    'style-loader', //css-loader 가 읽은 css 를 적용
+                    'css-loader' //css 파일을 읽는 로더
+                ]
+            }
+        ]
     }
 };
