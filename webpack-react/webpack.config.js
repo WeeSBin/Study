0b487bfd64a131630c6a29c87fb12b1b8993b79f
wesbin <tktma12>
2020-05-12 17:00:47 +0900

    웹팩-리액트 첫 커밋

---
+++ webpack-react/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.jsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname + "/build")
+    },
+    mode: "none",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: "/node_modules",
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
